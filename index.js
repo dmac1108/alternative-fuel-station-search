a9f51195d15e611a7a1111da6a0afa74f8222421
Dawn McNeil <dawn.a.mcneil@gmail.com>
2019-06-04 22:48:14 -0400

    add geolocation

--- index.js
+++ index.js
@@ -3,6 +3,43 @@
 const url="https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest";
 const APIkey = "BLnD9vbttpdXhyvlXImgyUXASEkcU2bUvSpa89fw";
 
+function handleLocation(latitude, longitude){
+    console.log("in the handleLocation function");
+    console.log("latitude: " + latitude + " longitude: " + longitude);
+}
+
+function getUserLocation(){
+    console.log("In the geolocation function");
+    geo = navigator.geolocation;
+    if ("geolocation" in navigator) {
+        console.log("Geolocation is available");
+      } else {
+          console.log("Geolocation is NOT available");
+      }
+   
+    geo.getCurrentPosition(function(position) {
+        handleLocation(position.coords.latitude, position.coords.longitude);
+      });
+}
+
+function displayList(responseJson){
+    console.log("in the displaylist function");
+    console.log(responseJson.fuel_stations);
+    for (let i=0; i<responseJson.fuel_stations.length; i++){
+        let stationName = responseJson.fuel_stations[i].station_name;
+        let distance = responseJson.fuel_stations[i].distance;
+        let fuel = responseJson.fuel_stations[i].fuel_type_code;
+        let accessCode = responseJson.fuel_stations[i].access_days_time;
+        let accessType = responseJson.fuel_stations[i].access_code;
+        let streetAddress = responseJson.fuel_stations[i].street_address;
+        let city = responseJson.fuel_stations[i].city;
+        appendString = `<h2>${stationName}</h2><p>${distance}</p><p>${fuel}</p><p>${accessCode}</p><p>${accessType}</p><p>${streetAddress}</p><p>${city}</p>`;
+        $('.js-result-list').append(appendString);
+
+    }
+}
+
+
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
@@ -26,7 +63,7 @@ function getData(location, radius, limit){
 
     fetch(queryUrl)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
+    .then(responseJson => displayList(responseJson))
     .catch(err => console.log(err));
     
 }
@@ -51,5 +88,6 @@ console.log("App Started");
 WatchForm();
 $('#radius').val(5);
 $('#limit').val(20);
+getUserLocation();
 
 });
